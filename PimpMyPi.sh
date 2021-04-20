a8b2c51a8c16c54be1ae1f79301456693c250f3d
bbaranoff <37385191+bbaranoff@users.noreply.github.com>
2021-04-20 08:37:26 +0200

    Update PimpMyPi.sh

--- PimpMyPi.sh
+++ PimpMyPi.sh
@@ -136,3 +136,21 @@ apt install libdbd-sqlite3
 mkdir /var/lib/osmocom
 wget https://github.com/bbaranoff/PImpMyPi/blob/main/osmo-msc.service
 cp osmo-msc.service /lib/systemd/system/osmo-msc.service
+
+
+
+
+Index: b/libs/miniwebrtc/typedefs.h
+===================================================================
+--- a/libs/miniwebrtc/typedefs.h
++++ b/libs/miniwebrtc/typedefs.h
+@@ -99,6 +99,9 @@
+ #define WEBRTC_ARCH_BIG_ENDIAN
+ #define WEBRTC_BIG_ENDIAN
+ #endif
++#elif defined(__aarch64__)
++#define WEBRTC_ARCH_64_BITS 
++#define WEBRTC_ARCH_LITTLE_ENDIAN 
+ 
+ #else
+ #error Please add support for your architecture in typedefs.h
